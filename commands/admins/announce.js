060d7f83c9117263c6a698bfc1352ef63ac9cb9d
Jason Vu <jvu@scu.edu>
2021-05-10 09:48:09 -0700

    Update announce.js

--- commands/admins/announce.js
+++ commands/admins/announce.js
@@ -34,12 +34,26 @@ module.exports = class announceCommand extends Command {
           key: "title",
           prompt: "Please provide some title text.",
           type: "string",
+          validate: (title) => {
+            if (title.length < 256) {
+              return true;
+            } else {
+                return "Please enter embed title under 256 characters!"
+            }
+          }
         },
 
         {
           key: "body",
           prompt: "Please provide some body text.",
           type: "string",
+          validate: (body) => {
+            if (body.length < 2048) {
+              return true;
+            } else {
+                return "Please enter embed description under 2048 characters!";
+            }
+          }
         },
         {
           key: "color",
@@ -47,14 +61,28 @@ module.exports = class announceCommand extends Command {
           type: "string",
         },
         {
-  	  key: "image",
-  	  prompt: "Please provide an image URL.",
-	  type: "string",
+          key: "image",
+          prompt: "Please provide an image URL.",
+          type: "string",
+          validate: (image) => {
+            if (image.match(/([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/i)) {
+              return true;
+            } else {
+                return "Please enter a proper image URL with the listed extensions!";
+            }
+          }
         },
         {
           key: "footer",
           prompt: "Please provide some footer text.",
           type: "string",
+          validate: (footer) => {
+            if (footer.length < 2048) {
+              return true;
+            } else {
+                return "Please enter embed footer under 2048 characters!";
+            }
+          }
         },
  
       ],
