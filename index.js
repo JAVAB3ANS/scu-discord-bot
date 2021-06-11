68d99c01866a043574a6fabe90881f6c51f99f02
Jason Vu <jvu@scu.edu>
2021-06-10 19:24:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -39,9 +39,7 @@ client.dispatcher.addInhibitor( (client, msg) => {
         return true; 
     } 
   } catch(err) {
-      if (err === "TypeError: Inhibitor \"\" had an invalid result; must be a string or an Inhibition object.") {
-        return;
-      }
+      if (err === "TypeError: Inhibitor \"\" had an invalid result; must be a string or an Inhibition object.") return; 
   }
 }); 
 
