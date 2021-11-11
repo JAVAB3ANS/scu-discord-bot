63cb03a10ca2dfd5e9169b19864ad5b6600253fd
Jason Vu <jvu@scu.edu>
2021-11-11 11:23:04 -0800

    fix code

--- index.js
+++ index.js
@@ -25,22 +25,24 @@ client.registry
   })
   .registerCommandsIn(path.join(__dirname, "commands")); 
 
-  client.dispatcher.addInhibitor( (client, msg) => {
-    try { 
-      switch (msg.command.group.name) {
-        case "Admins":
-          if (!client.config.serverRoles.modRoles.forEach((modRole) => msg.member.roles.cache.has(modRole)) || !msg.author.id === client.config.serverRoles.owner) {
-            client.error(`***<@${msg.author.id}>, You don't have permission to use this command***`, msg);
-            msg.delete();
-            return false;
-          }
-          break;
-        default:
-          return true; 
-      } 
-    } catch(err) {
-        if (err === "TypeError: Inhibitor \"\" had an invalid result; must be a string or an Inhibition object.") return; 
-    }
+client.dispatcher.addInhibitor( (client, msg) => {
+  try { 
+    switch (msg.command.group.name) {
+      case "Admins":
+        if (client.config.serverRoles.modRoles.forEach((modRole) => msg.member.roles.cache.has(modRole)) || msg.author.id === client.config.serverRoles.owner) {
+          return false;
+        } else {
+            client.error(`***<@${msg.author.id}>, You don't have permission to use this command***`, msg); 
+            return "Invalid permissions!";
+        }
+      default:
+        return false; 
+    } 
+  } catch(err) {
+      if (err === "TypeError: Inhibitor \"\" had an invalid result; must be a string or an Inhibition object.") {
+        return;
+      }
+  }
 }); 
 
 client.once("ready", () => {
@@ -70,4 +72,3 @@ client
     .on("guildMemberAdd", (member) => require("./events/guildMemberAdd")(client, member));
  
 client.login(client.config.token); 
- 
