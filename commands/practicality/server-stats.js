3d022b65937e5109bbd7d7ac40b9721e91ef4b83
Jason Vu <jvu@scu.edu>
2022-04-15 21:06:07 -0700

    fix server-stats.js

--- commands/practicality/server-stats.js
+++ commands/practicality/server-stats.js
@@ -35,9 +35,8 @@ module.exports = class serverStatsCommand extends Command {
 
 			let serverEmbed = new MessageEmbed()
 			.setDescription(`__**${message.guild.name} - Statistics**__`)
-			.setColor(this.client.config.default_color)
-			.addField("Server Owner", `${message.guild.owner}`, true)
-			.addField("Server Region", message.guild.region, true) 
+			.setColor(this.client.config.school_color)
+			.addField("Server Region", message.guild.preferredLocale, true) 
 			.addField("Server Name", message.guild.name, true)
 			.addField("Verification level", message.guild.verificationLevel, true)
 			.addField("Channel Count", message.guild.channels.cache.size, true)
@@ -45,7 +44,7 @@ module.exports = class serverStatsCommand extends Command {
 			.addField("Humans", checkMembers(message.guild), true)
 			.addField("Bots", checkBots(message.guild), true)
 			.addField("Guild Created At:", message.guild.createdAt, true)
-			.addField("Roles", message.guild.roles.cache.map(x =>x.name), true)
+			.addField("Total Amount of Roles", message.guild.roles.cache.size, true)
 			.setTimestamp();
  
 			message.channel.send(serverEmbed); 
