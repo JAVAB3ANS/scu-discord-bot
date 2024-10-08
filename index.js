cd182ce5e000da63ca205daad825d0e0d73ef6a8
JAVAB3ANS <jvu@scu.edu>
2024-10-08 10:14:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ const { log } = require("./functions/log.js");
 client.registry
   .registerDefaultTypes()
   .registerGroups([  
-    ["admins", "Admins"],
-    ["fun", "Fun"],
     ["practicality", "Practicality"],
   ])
   .registerDefaultGroups()
@@ -23,27 +21,7 @@ client.registry
     unknownCommand: false,
     prefix: false, 
   })
-  .registerCommandsIn(path.join(__dirname, "commands")); 
-
-client.dispatcher.addInhibitor( (client, msg) => {
-  try { 
-    switch (msg.command.group.name) {
-      case "Admins":
-        if (client.config.serverRoles.modRoles.forEach((modRole) => msg.member.roles.cache.has(modRole)) || msg.author.id === client.config.serverRoles.owner) {
-          return false;
-        } else {
-            client.error(`***<@${msg.author.id}>, You don't have permission to use this command***`, msg); 
-            return "Invalid permissions!";
-        }
-      default:
-        return false; 
-    } 
-  } catch(err) {
-      if (err === "TypeError: Inhibitor \"\" had an invalid result; must be a string or an Inhibition object.") {
-        return;
-      }
-  }
-}); 
+  .registerCommandsIn(path.join(__dirname, "commands"));  
 
 client.once("ready", () => {
   client.user.setPresence({activity: { name: `${client.config.prefix}help || DM me for help!` }, status: "online"});  
